a7e5f0f16b5bc605a55f387c526a865145ac578f
headlessguy123 <ethfeiaws@proton.me>
2025-02-11 14:53:50 +0800

    initial commit

--- spider.py
+++ spider.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-
+import os
 import json
+from config import basePath
 
 def load_video():
-    with open('videos.json', 'r', encoding='utf-8') as f:
+    with open(os.path.join(basePath, 'videos.json'), 'r', encoding='utf-8') as f:
         data = json.load(f)
     return data
